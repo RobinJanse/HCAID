5b6ca90ab54124ca0c82ce5a592820485ba98b70
Stan Barkmeijer <stan@barkmeijer.com>
2023-11-03 09:44:33 +0100

    Tailwind install

--- django/hcaidApi/hcaidApi/settings.py
+++ django/hcaidApi/hcaidApi/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "tailwind",
+    'hcaidApi', # Apparently this is needed to make the app work
 ]
 
 MIDDLEWARE = [
