f2d1a5df21d546bfaceba3a1b9eba8aa5c5c25b6
Stan <stanbarkmeijer@hotmail.com>
2023-11-05 17:15:25 +0100

    Bad model works too now

--- django/hcaidApi/hcaidApi/app/bad/AI/model.py
+++ django/hcaidApi/hcaidApi/app/bad/AI/model.py
@@ -20,25 +20,17 @@ class AIModelBad:
 
     def predict_bad(self, age, gender, country, seek_help, tech_company, remote_work):
         print("Predicting bad employee")
-        print(age)
 
-        X = np.array([0, 0, 0.4, 0, 0, 0]).reshape(1, -1)
+        le = LabelEncoder()
+        
+        gender_encoded = le.fit_transform([gender])[0]
+        country_encoded = le.fit_transform([country])[0]
+        
+        X = np.array([[age, gender_encoded, country_encoded, seek_help, tech_company, remote_work]])
+        prediction = self.rfc_model.predict(X)
+        prediction_list = prediction.tolist()
 
-        print("X: ", X)
-
-        #X = self.scaler.transform(X)
-        print("Using ANN model")
-        prediction = self.ann_model.predict(X)
-        print("Using SVC model: ", prediction)
-        if(prediction == 1):
-            prediction = self.svc_model.predict(X)
-            print("Using SVC model: ", prediction)
-
-        if(prediction < 0.5):
-            prediction = 0
-        else:
-            prediction = 1
-        return prediction
+        return prediction_list[0]
 
     def predict_good(self, age, gender, country, seek_help, tech_company, remote_work):
         print("Predicting using the good model")
