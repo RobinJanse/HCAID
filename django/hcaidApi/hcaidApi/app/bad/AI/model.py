5b6d56a40b2d1a78940c95a9066d423d9bc4e937
Stan <stanbarkmeijer@hotmail.com>
2024-01-19 17:39:38 +0100

    New model, new me

--- django/hcaidApi/hcaidApi/app/bad/AI/model.py
+++ django/hcaidApi/hcaidApi/app/bad/AI/model.py
@@ -13,7 +13,7 @@ class AIModelBad:
             print("Loading model from file: ", filename)
             return pickle.load(open(filename, 'rb'))
         
-        self.rfc_model = load_model(os.path.join(os.getcwd(), 'Model/rfc_model.pkl'))
+        self.rfc_model = load_model(os.path.join(os.getcwd(), 'hcaidApi\\app\\bad\\AI\\models\\mental_health_model.pkl'))
 
     def predict_bad(self, age, gender, country, seek_help, tech_company, remote_work):
         print("Predicting bad employee")
@@ -29,15 +29,32 @@ class AIModelBad:
 
         return prediction_list[0]
 
-    def predict_good(self, age, gender, country, seek_help, tech_company, remote_work):
+    def predict_good(self, 
+        employer_mental_health_benefits, 
+        awareness_of_mental_health_coverage, 
+        employer_discussed_mental_health, 
+        employer_mental_health_resources, 
+        anonymity_protection, 
+        ease_of_medical_leave_for_mental_health,
+        employer_react_negative_mental_health,
+        employer_seriousness_mental_vs_physical,
+        observed_consequences_mental_health,
+        mental_health_impact_on_productivity
+    ):
         print("Predicting using the good model")
-
-        le = LabelEncoder()
         
-        gender_encoded = le.fit_transform([gender])[0]
-        country_encoded = le.fit_transform([country])[0]
-        
-        X = np.array([[age, gender_encoded, country_encoded, seek_help, tech_company, remote_work]])
+        X = np.array([[
+            employer_mental_health_benefits, 
+            awareness_of_mental_health_coverage, 
+            employer_discussed_mental_health, 
+            employer_mental_health_resources, 
+            anonymity_protection, 
+            ease_of_medical_leave_for_mental_health,
+            employer_react_negative_mental_health,
+            employer_seriousness_mental_vs_physical,
+            observed_consequences_mental_health,
+            mental_health_impact_on_productivity
+        ]])
         prediction = self.rfc_model.predict(X)
         prediction_list = prediction.tolist()
         
--- django/hcaidApi/hcaidApi/app/forms.py
+++ django/hcaidApi/hcaidApi/app/forms.py
@@ -8,28 +8,218 @@ class NameForm(forms.Form):
     name = forms.CharField(label='Your name', max_length=100)
 
 class BadApplyForm(forms.Form):
-    name = forms.CharField(label='Your name', max_length=100, required=True)
-    email = forms.EmailField(label='Your email', max_length=100, required=True)
-    
-    age = forms.CharField(label='Age', max_length=100, required=True)
-    gender = forms.BooleanField(label='Is Male', required=False)
-    country = forms.CharField(label='Country', max_length=100, required=True)
-    seek_help = forms.BooleanField(label='Ever got mental help', required=False)
-    tech_company = forms.BooleanField(label='Last worked at company was tech company', required=False)
-    remote_work = forms.BooleanField(label='Did you work remotely at your last company', required=False)
-    accept_terms = forms.BooleanField(label=mark_safe('I accept the Privacy Policy'), required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
+    employer_mental_health_benefits = forms.ChoiceField(
+        choices=[
+            (3, 'Yes'),
+            (0, "I don't know"),
+            (2, 'No'),
+            (1, "Not eligible for coverage / N/A"),
+        ],
+        label='Does your employer provide mental health benefits?',
+        required=True,
+    )
+
+    awareness_of_mental_health_coverage = forms.ChoiceField(
+        choices=[
+            (2, 'Yes'),
+            (1, 'No'),
+            (0, "I am not sure"),
+        ],
+        label='Are you aware of the options for mental health care provided by your employer?',
+        required=True,
+    )
+
+    employer_discussed_mental_health = forms.ChoiceField(
+        choices=[
+            (2, 'Yes'),
+            (1, 'No'),
+            (0, "I don't know"),
+        ],
+        label='Has your employer ever discussed mental health as part of an employee wellness program?',
+        required=True,
+    )
+
+    employer_mental_health_resources = forms.ChoiceField(
+        choices=[
+            (2, 'Yes'),
+            (1, 'No'),
+            (0, "I don't know"),
+        ],
+        label='Does your employer provide resources to learn more about mental health issues and how to seek help?',
+        required=True,
+    )
+
+    anonymity_protection = forms.ChoiceField(
+        choices=[
+            (2, 'Yes'),
+            (1, 'No'),
+            (0, "I don't know"),
+        ],
+        label='Is your anonymity protected if you choose to take advantage of mental health or substance abuse treatment resources?',
+        required=True,
+    )
+
+    ease_of_medical_leave_for_mental_health = forms.ChoiceField(
+        choices=[
+            (2, 'Somewhat difficult'),
+            (5, 'Very easy'),
+            (3, "Somewhat easy"),
+            (4, "Very difficult"),
+            (0, "I don't know"),
+            (1, "Neither easy nor difficult"),
+        ],
+        label='If a mental health issue prompted you to request a medical leave from work, asking for that leave would be:',
+        required=True,
+    )
+
+    employer_react_negative_mental_health = forms.ChoiceField(
+        choices=[
+            (2, 'Yes'),
+            (1, 'No'),
+            (0, "Maybe"),
+        ],
+        label='Do you think that discussing a mental health disorder with your employer would have negative consequences?',
+        required=True,
+    )
+
+    employer_seriousness_mental_vs_physical = forms.ChoiceField(
+        choices=[
+            (2, 'Yes'),
+            (1, 'No'),
+            (0, "I don't know"),
+        ],
+        label='Do you think that your employer takes mental health as seriously as physical health?',
+        required=True,
+    )
+
+    observed_consequences_mental_health = forms.ChoiceField(
+        choices=[
+            (1, 'Yes'),
+            (0, 'No'),
+        ],
+        label='Have you heard of or observed negative consequences for coworkers with mental health conditions in your workplace?',
+        required=True,
+    )
+
+    mental_health_impact_on_productivity = forms.ChoiceField(
+        choices=[
+            (3, 'Yes'),
+            (0, 'No'),
+            (2, "Unsure"),
+            (1, "Not applicable to me"),
+        ],
+        label='Do you think that discussing a mental health disorder with your employer would have negative consequences?',
+        required=True,
+    )
+
+    privacy_box = forms.BooleanField(label=mark_safe('I accept the Privacy Policy'), required=True, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
 
 class GoodApplyForm(forms.Form):
-    name = forms.CharField(label='Your name*', max_length=100, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(label='Your email*', max_length=100, required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    employer_mental_health_benefits = forms.ChoiceField(
+        choices=[
+            (3, 'Yes'),
+            (0, "I don't know"),
+            (2, 'No'),
+            (2, "Not eligible for coverage / N/A"),
+        ],
+        label='Does your employer provide mental health benefits?',
+        required=True,
+    )
 
-    age = forms.IntegerField(label='Age*', required=True, widget=forms.NumberInput(attrs={'class': 'form-control'}))
-    gender = forms.CharField(label="Gender", max_length=100, required=False)
-    country = forms.CharField(label='Country*', max_length=100, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    seek_help = forms.BooleanField(label='Ever got mental help', required=False)
-    tech_company = forms.BooleanField(label='Last worked at company was tech company', required=False)
-    remote_work = forms.BooleanField(label='Did you work remotely at your last company', required=False)
-    accept_terms = forms.BooleanField(label=mark_safe('I accept the Privacy Policy*'), required=True, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
+    awareness_of_mental_health_coverage = forms.ChoiceField(
+        choices=[
+            (2, 'Yes'),
+            (1, 'No'),
+            (0, "I am not sure"),
+        ],
+        label='Are you aware of the options for mental health care provided by your employer?',
+        required=True,
+    )
+
+    employer_discussed_mental_health = forms.ChoiceField(
+        choices=[
+            (2, 'Yes'),
+            (1, 'No'),
+            (0, "I don't know"),
+        ],
+        label='Has your employer ever discussed mental health as part of an employee wellness program?',
+        required=True,
+    )
+
+    employer_mental_health_resources = forms.ChoiceField(
+        choices=[
+            (2, 'Yes'),
+            (1, 'No'),
+            (0, "I don't know"),
+        ],
+        label='Does your employer provide resources to learn more about mental health issues and how to seek help?',
+        required=True,
+    )
+
+    anonymity_protection = forms.ChoiceField(
+        choices=[
+            (2, 'Yes'),
+            (1, 'No'),
+            (0, "I don't know"),
+        ],
+        label='Is your anonymity protected if you choose to take advantage of mental health or substance abuse treatment resources?',
+        required=True,
+    )
+
+    ease_of_medical_leave_for_mental_health = forms.ChoiceField(
+        choices=[
+            (2, 'Somewhat difficult'),
+            (5, 'Very easy'),
+            (3, "Somewhat difficult"),
+            (4, "Very difficult"),
+            (0, "I don't know"),
+            (1, "Neither easy nor difficult"),
+        ],
+        label='If a mental health issue prompted you to request a medical leave from work, asking for that leave would be:',
+        required=True,
+    )
+
+    employer_react_negative_mental_health = forms.ChoiceField(
+        choices=[
+            (2, 'Yes'),
+            (1, 'No'),
+            (0, "Maybe"),
+        ],
+        label='Do you think that discussing a mental health disorder with your employer would have negative consequences?',
+        required=True,
+    )
+
+    employer_seriousness_mental_vs_physical = forms.ChoiceField(
+        choices=[
+            (2, 'Yes'),
+            (1, 'No'),
+            (0, "I don't know"),
+        ],
+        label='Do you think that your employer takes mental health as seriously as physical health?',
+        required=True,
+    )
+
+    observed_consequences_mental_health = forms.ChoiceField(
+        choices=[
+            (1, 'Yes'),
+            (0, 'No'),
+        ],
+        label='Have you heard of or observed negative consequences for coworkers with mental health conditions in your workplace?',
+        required=True,
+    )
+
+    mental_health_impact_on_productivity = forms.ChoiceField(
+        choices=[
+            (3, 'Yes'),
+            (0, 'No'),
+            (2, "Unsure"),
+            (1, "Not applicable to me"),
+        ],
+        label='Do you think that discussing a mental health disorder with your employer would have negative consequences?',
+        required=True,
+    )
+
+    privacy_box = forms.BooleanField(label=mark_safe('I accept the Privacy Policy'), required=True, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
 
 class GoodPredictionForm(forms.ModelForm):
     class Meta:
