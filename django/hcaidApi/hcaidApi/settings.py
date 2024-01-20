5d9764475ca72f1a521aeb1bc1f5905a1d4a25d4
Stan <stanbarkmeijer@hotmail.com>
2024-01-20 20:06:07 +0100

    Adjust settings for Heroku deployment

--- django/hcaidApi/hcaidApi/settings.py
+++ django/hcaidApi/hcaidApi/settings.py
@@ -14,12 +14,11 @@ from pathlib import Path
 import os
 import django_heroku
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
